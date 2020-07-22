e78ab1e8d71699aab9b393c681428f3643c41c16
kranonetka <fakemail4steam@ya.ru>
2020-07-23 00:59:44 +0700

    [#] Init db fix

--- init_db.py
+++ init_db.py
@@ -7,5 +7,10 @@ if __name__ == "__main__":
     if not os.path.isfile("duty_bot/" + DB_FILE):
         db.create_all()
         db.session.bulk_save_objects((DutyRooms(room=i) for i in range(601, 639)))
-        db.session.add(SyncTable(date=datetime(year=2020, month=4, day=17), left_room=610, right_room=637))
+        db.session.merge(SyncTable(
+            id=0,
+            date=datetime(year=2020, month=4, day=17),
+            left_room=610,
+            right_room=637)
+        )
         db.session.commit()
