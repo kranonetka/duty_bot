62727a30f9b3b28fbde3588f5369bb8f70b561ec
kranonetka <fakemail4steam@ya.ru>
2020-07-23 09:44:24 +0700

    [#] Changed owners

--- duty_bot/__init__.py
+++ duty_bot/__init__.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 GROUP_ID = 192644739
-OWNERS6_6 = (227725150, 138443566, 299443070, 180583820, 259344328)
+OWNERS6_6 = (246650048,)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
