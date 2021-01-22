0e6dddd1d2cb29a91f447d6ac270a8509bab277d
kranonetka <fakemail4steam@ya.ru>
2021-01-22 21:05:41 +0700

    refac: mode as positional arg

--- Parser/_grammar/__init__.py
+++ Parser/_grammar/__init__.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 import parsimonious
 
-with (Path(__file__).parent / 'message.grammar').open(mode='r', encoding='utf-8') as fp:
+with (Path(__file__).parent / 'message.grammar').open('r', encoding='utf-8') as fp:
     message_grammar = parsimonious.Grammar(fp.read())
