8bf17da4ff26dcd052a30c02531940f73ced48d4
kranonetka <fakemail4steam@ya.ru>
2021-01-22 22:11:46 +0700

    fix: missing Message cls

---
+++ VkBot/Parser/_message.py
@@ -0,0 +1,31 @@
+if False:  # Type hinting
+    from ._mention import Mention  # noqa
+    from ._commands import Command
+    from typing import Optional
+
+
+class Message:
+    def __init__(self, command, mention=None):
+        """
+        :type command: Command
+        :type mention: Optional[Mention]
+        """
+        self._command = command
+        self._mention = mention
+
+    @property
+    def command(self):  # type: () -> Command
+        return self._command
+
+    @property
+    def mention(self):  # type: () -> Mention
+        return self._mention
+
+    @mention.setter
+    def mention(self, mention):  # type: (Mention) -> None
+        if isinstance(mention, Mention):
+            self._mention = mention
+        raise TypeError(mention.__class__.__name__)
+
+    def __repr__(self):
+        return f'Message({self._mention}, {self._command})'
